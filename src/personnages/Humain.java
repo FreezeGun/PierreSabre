9d992e83f6e9252c859e86adf6022cb8bf226789
FreezeGun <benaounali@gmail.com>
2023-03-28 16:43:12 +0200

    tp

---
+++ src/personnages/Humain.java
@@ -0,0 +1,52 @@
+package personnages;
+
+public class Humain {
+	 	private String nom;
+	    private String boisson;
+	    private int argent;
+	    
+	    public Humain(String nom, String boisson, int argent) {
+	        this.nom = nom;
+	        this.boisson = boisson;
+	        this.argent = argent;
+	    }
+	    
+	    public String getNom() {
+	        return nom;
+	    }
+	    
+	    public int getArgent() {
+	        return argent;
+	    }
+	    
+	    public void direBonjour() {
+	        parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boisson + ".");
+	    }
+	    
+	    public void boire() {
+	        parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	    }
+	    
+	    public void acheter(String bien, int prix) {
+	        if (argent >= prix) {
+	            argent -= prix;
+	            parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+	        } else {
+	            parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un " + bien +" à " + prix);
+	        }
+	    }
+	    
+	    public void gagnerArgent(int gain) {
+	        argent += gain;
+	        parler("Je gagne " + gain + " yen.");
+	    }
+	    
+	    public void perdreArgent(int perte) {
+	        argent -= perte;
+	        parler("Je perds " + perte + " yen.");
+	    }
+	    
+	    private void parler(String texte) {
+	        System.out.println(nom + " : " + texte);
+	    }
+}
